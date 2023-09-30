71cd87ed1c9cb160491e6c2fc1b7dc6465412d91
Filmymoviechannel <144339160+Filmymoviechannel@users.noreply.github.com>
2023-09-30 15:11:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -13,7 +13,7 @@ def is_enabled(value, default):
 # Bot information
 SESSION = environ.get('SESSION', 'Media_search')
 API_ID = int(environ.get('API_ID', ''))
-API_HASH = environ.get('API_HASH', '')
+API_HASH = environ.get('API_HASH', '88cea0cbf0e1e1f943ae082544076519')
 BOT_TOKEN = environ.get('BOT_TOKEN', '')
 # Bot settings
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
